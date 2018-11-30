5582e6893158b7524b8903d7a89365d4bd932cfd
gismayilov <guseynism@gmail.com>
2018-11-30 14:28:39 +0300

    fixed requires

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 let exportsObj = require('./elms');
 exportsObj.CreatedElement = require('./CreatedElement');
 exportsObj.ElementWithAppendedChildren = require('./ElementWithAppendedChildren');
-exportsObj.ElementWithAppendedText = require('./ElementWithText');
+exportsObj.ElementWithAppendedText = require('./ElementWithAppendedText');
 exportsObj.ElementWithAttributes = require('./ElementWithAttributes');
 exportsObj.ElementWithInnerHTML = require('./ElementWithInnerHTML');
 exportsObj.ElementWithTextContent = require('./ElementWithTextContent');
